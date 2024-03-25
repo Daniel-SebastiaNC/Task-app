84f13af7cb56f1ac507746c671b9048aa1be4d34
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-03-25 16:02:44 -0300

    add remove and modal

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { ImageBackground, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View, YellowBox, Modal, TouchableHighlight, TextInput} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { useState } from 'react';
 
+console.dissableYellowBox = true;
 
 export default function App() {
 
   const img = require('./resources/bg.jpg');
+
+  const [modal,setModal] = useState(true);
 
   const [tasks, setTasks] = useState([
     {
@@ -31,6 +34,12 @@ export default function App() {
 
   function deleteItem(posicion){
     alert(posicion)
+
+    let newTasks = tasks.filter((val) =>{
+      return val.id != posicion;
+    })
+
+    setTasks(newTasks);
   }
 
   function ComponentList(props){
@@ -50,6 +59,29 @@ export default function App() {
   return (
     <ScrollView style={styles.body}>
       <Header></Header>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput autoFocus={true}></TextInput>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModal(!modal);
+              }}
+            >
+              <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
       {
 
         tasks.map(function(val){      
@@ -57,6 +89,7 @@ export default function App() {
         })
 
       }
+
     </ScrollView>
   );
 }
@@ -89,5 +122,43 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center'
   },
+  //Estilos para nossa modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'rgba(0,0,0,0.5)'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex:5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 
 });
